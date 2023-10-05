8b8d3364ccbfc85e0c6d7c11ca395791212d2046
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2023-10-05 15:08:18 +0300

    Fixed error with nextInt

--- src/main/java/lotr/NobleKick.java
+++ src/main/java/lotr/NobleKick.java
@@ -6,7 +6,7 @@ public class NobleKick implements KickStrategy{
      private static Random rand = new Random();
     @Override
     public void kick(Character whoKick, Character opponent) {
-            opponent.setHp(opponent.getHp() - rand.nextInt(1,whoKick.getPower()));
+            opponent.setHp(opponent.getHp() - rand.nextInt(whoKick.getPower()));
         
     }
     
