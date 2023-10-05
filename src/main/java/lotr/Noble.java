e7cd432067ace3ae02de8f52018d2c32e9ef2778
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2023-10-05 15:10:03 +0300

    Added static rand

--- src/main/java/lotr/Noble.java
+++ src/main/java/lotr/Noble.java
@@ -4,10 +4,12 @@ import java.util.Random;
 
 
 public abstract class Noble extends Character{
+    private static Random rand = new Random();
+    
     
     public Noble(int min, int max, KickStrategy strategy){
         
-        super(new Random().nextInt(max - min) + min + 1, new Random().nextInt(max - min) + min + 1, strategy);
+        super(rand.nextInt(max - min) + min + 1, rand.nextInt(max - min) + min + 1, strategy);
     }
 
 }
